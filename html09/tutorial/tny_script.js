b9742cbbb42443663890d5fb9790a2b113f7ec95
isaacsfluis <isaacsfluis@gmail.com>
2024-08-01 15:00:22 -0400

    Session 9.2

--- html09/tutorial/tny_script.js
+++ html09/tutorial/tny_script.js
@@ -13,3 +13,22 @@
 // window.alert("Welcome to Tulsa");
 // Window.Alert("Welcome to Tulsa");
 
+/* Display the current date and time */
+document.getElementById("dateNow").innerHTML = "m/d/y<br />h:m:s";
+// document.getElementById("dateNow").innerHTML = "hola";
+
+
+/* Display the time left until New Year's Eve */
+document.getElementById("days").textContent = "dd";
+document.getElementById("hrs").textContent = "hh";
+document.getElementById("mins").textContent = "mm";
+document.getElementById("secs").textContent = "ss";
+
+
+/* Store the current date and time */
+var currentDay = new Date("May 23, 2018 14:35:05");
+
+var dateStr = currentDay.toLocaleDateString();
+var timeStr = currentDay.toLocaleTimeString();
+
+document.getElementById("dateNow").innerHTML = dateStr + "<br />" + timeStr;
