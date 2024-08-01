4a3304c5a2ca008f2aafa13f69bac56e006a2859
isaacsfluis <isaacsfluis@gmail.com>
2024-08-01 12:36:13 -0400

    Session 9.1

---
+++ html09/tutorial/tny_script.js
@@ -0,0 +1,15 @@
+"use strict";
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Tutorial Case
+
+   Countdown Clock
+   Author: Luis Isaacs
+   Date:   07/30/2024
+
+*/
+
+// window.alert("Welcome to Tulsa");
+// Window.Alert("Welcome to Tulsa");
+
